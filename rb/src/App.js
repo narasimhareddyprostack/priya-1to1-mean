55cfa6ab63975c5d3107eb3734caeb906af501fd
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-10-27 08:38:41 +0530

    react

---
+++ rb/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <h2>Hello .. Good Morning</h2>;
+}
+
+export default App;
+//JSX  - HTML + XML(JS)
+//npx
+
+// {{angular}}
+
+// { React}
+
+// <%= EJS %>
+// <%php %>
