b7fd1a95fb9ed45a2e51179ccf80bd05a5da4d64
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-10-28 08:58:05 +0530

    good

---
+++ rb/src/Order.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Order(props) {
+  return <h3> {props.noProducts}Orders Delivered Succussfully </h3>;
+}
+export default Order;
+//your Javascript - does understand HTML
+//React component
--- rb/src/index.js
+++ rb/src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Order from "./Order";
+import Product from "./component/product";
 
-ReactDOM.render(<App />, document.getElementById("priya"));
+ReactDOM.render(<Product />, document.getElementById("priya"));
